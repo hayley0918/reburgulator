d1296309f14eb532ef37bf3a86b365638237af28
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-06-06 08:25:23 +1000

    Updates

---
+++ precache-manifest.3627ef520a89ce5b8e9545dbf876001c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351532c5ab62b0eb1e5e5cb8f3636646",
+    "url": "/reburgulator/index.html"
+  },
+  {
+    "revision": "7b72e060d9c299650344",
+    "url": "/reburgulator/static/css/main.4e9d8c2b.chunk.css"
+  },
+  {
+    "revision": "4467e00b0b89a5d4d72e",
+    "url": "/reburgulator/static/js/2.ba0e5091.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reburgulator/static/js/2.ba0e5091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b72e060d9c299650344",
+    "url": "/reburgulator/static/js/main.9d22048e.chunk.js"
+  },
+  {
+    "revision": "2a5be8d2024c3fb3705c",
+    "url": "/reburgulator/static/js/runtime-main.8259f070.js"
+  }
+]);
